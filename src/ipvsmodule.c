460623181551c8ce55d5cebccd2b012ff8084220
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-01-04 20:29:13 +0100

    Display size of connection hash table

--- src/ipvsmodule.c
+++ src/ipvsmodule.c
@@ -7,8 +7,14 @@ static PyObject *ipvs_nr_services(PyObject *self, PyObject *args)
     return Py_BuildValue("l", ipvs_info.num_services);
 }
 
+static PyObject *ipvs_hashtable_size(PyObject *self, PyObject *args)
+{
+    return Py_BuildValue("l", ipvs_info.size);
+}
+
 static PyMethodDef module_methods[] = {
     {"nr_services", ipvs_nr_services, METH_NOARGS, "get number of services"},
+    {"hashtable_size", ipvs_hashtable_size, METH_NOARGS, "get size of connection hashtable"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
--- test_ipvs.py
+++ test_ipvs.py
@@ -4,3 +4,6 @@ print dir(ipvs)
 
 print "nr services:", ipvs.nr_services()
 print "nr services:", ipvs.nr_services.__doc__
+
+print "hash table size:", ipvs.hashtable_size()
+print "hash table size:", ipvs.hashtable_size.__doc__
